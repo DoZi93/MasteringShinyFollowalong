ad79ffa0410e896ee9cf39bef305ac0d00c50c48
Dominik Ziaja <dominik.ziaja@uni-bielefeld.de>
2022-11-06 18:59:49 +0100

    continued the exercises in the BasicUI section.

--- Chapter2/02_BasicUI_2_3_5.R
+++ Chapter2/02_BasicUI_2_3_5.R
@@ -1,10 +1,19 @@
 library(shiny)
 
 ui <- fluidPage(
+  verbatimTextOutput("mtcars_summary"), # Number 1 
+  textOutput("greeting"), # Number 2 
+  verbatimTextOutput("ttest"), # Number 3 
+  verbatimTextOutput("linmodel1") # Number 4 
   
   )
   
 server <- function(input, output, session){
-  
+  # Should these renders be paired with textOutput or verbatimTextOutput?
+  output$mtcars_summary <- renderPrint(summary(mtcars)) # Number 1 
+  output$greeting <- renderText("Good morning!") # Number 2 
+  output$ttest <- renderPrint(t.test(1:5, 2:6)) # Number 3 
+  output$linmodel1 <- renderText(str(lm(mpg ~ wt, data = mtcars))) # Number 4 
 }
+
 shinyApp(ui, server)
